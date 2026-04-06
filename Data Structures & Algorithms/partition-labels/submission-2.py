cf2f89784c491a01f533daca4928e8ede6dbd69c
Joshua Silva <72359611+joshuasilva414@users.noreply.github.com>
2026-04-06 10:31:46 -0500

    Add: partition-labels - submission-2

---
+++ Data Structures & Algorithms/partition-labels/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        n=len(s)
+        def chToIdx(ch) -> int:
+            return ord(ch)-ord('a')
+
+        first: List[int] = [-1] * 26
+        last: List[int] = [-1] * 26
+        for i in range(n):
+            if first[chToIdx(s[i])]==-1:
+                first[chToIdx(s[i])]=i
+            last[chToIdx(s[i])]=i
+        
+        res = []
+        l=0
+        r=last[chToIdx(s[0])]
+        i=l
+
+        while True:
+            while i < r and r <= n-1:
+                r = max(r, last[chToIdx(s[i])])
+                i+=1
+            res.append(r-l+1)
+            i+=1
+            l=i
+            if i>=n:
+                break
+            r=last[chToIdx(s[i])]
+            if r > n-1:
+                break
+        return res
